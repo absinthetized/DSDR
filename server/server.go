3ba9f7d7f6916c458b0db6ac8805a8d8c52b039a
Matteo Nunziati <eng.matteo.nunziati@gmail.com>
2021-10-24 13:08:18 +0200

    new go server has feature parity with old python stub

--- server/server.go
+++ server/server.go
@@ -1,6 +1,12 @@
 package main
 
 import (
+	"encoding/json"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"os"
+
 	"github.com/gin-gonic/gin"
 )
 
@@ -8,18 +14,63 @@ func main() {
 	// start default gin middleware
 	r := gin.Default()
 
-	// map static resources from svelte
+	// serve static resources from svelte
 	r.Static("/svelte", "./static/build")
 	r.StaticFile("/favicon.png", "./static/favicon.png")
 	r.StaticFile("/global.css", "./static/global.css")
 	r.StaticFile("/", "./static/index.html")
 
-	// just a debug end point to be removed
-	r.GET("/ping", func(c *gin.Context) {
-		c.JSON(200, gin.H{
-			"message": "pong",
-		})
+	// serve the mock DB to the frontend
+	r.GET("/roles", func(c *gin.Context) {
+		roles, err := db_parser()
+		if err != nil {
+			c.JSON(http.StatusInternalServerError, gin.H{"error": err})
+		}
+
+		c.JSON(http.StatusOK, roles)
 	})
 
 	r.Run() // listen and serve on 0.0.0.0:8080
+}
+
+func db_parser() ([]role, error) {
+	role_dir := "./roles"
+	files, err := ioutil.ReadDir(role_dir)
+
+	if err != nil {
+		log.Print(err)
+		return nil, err
+	}
+
+	var roles []role
+
+	for id, file := range files {
+		// read file
+		data, err := ioutil.ReadFile(role_dir + string(os.PathSeparator) + file.Name())
+		if err != nil {
+			log.Print(err)
+			return nil, err
+		}
+
+		var role role
+		err = json.Unmarshal(data, &role)
+		if err != nil {
+			log.Print(err)
+			return nil, err
+		}
+
+		role.Id = id
+		roles = append(roles, role)
+	}
+
+	return roles, nil
+}
+
+type role struct {
+	Description         string   `json:"description"`
+	Name                string   `json:"name"`
+	Stage               string   `json:"stage"`
+	Title               string   `json:"title"`
+	IncludedPermissions []string `json:"includedPermissions"`
+	Id                  int      `json:"id"`
 }
