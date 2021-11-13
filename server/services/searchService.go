4bb68999215fc3f1e987a225312690c26edcf5ad
Matteo Nunziati <eng.matteo.nunziati@gmail.com>
2021-11-13 16:05:35 +0100

    server: patched code to fit in the repositry pattern

--- server/services/searchService.go
+++ server/services/searchService.go
@@ -3,11 +3,18 @@ package services
 import (
 	data "dsdr/data"
 	models "dsdr/models"
+	"sort"
+	"strings"
 )
 
 // SearchRole returns the roles whose permissions match the provided searchString
 // searchString is a string of space separated search terms
-func SearchRole(searchString string) ([]models.Role, error) {
-	//just fake the search for now and simply load the full database
-	return data.DB_parser()
+func SearchRole(searchString string, repo *data.RoleRepository) ([]models.Role, error) {
+	// separate the search terms in a slice
+	items := strings.Split(searchString, " ")
+	// sort search so that, if not all the criteria are matched we can order
+	// alphabetically by first maching term later
+	sort.Strings(items)
+
+	return repo.SearchFor(items), nil //check if err is of any usage in this context..
 }
