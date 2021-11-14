a1ad1837d76716bd89c8cca6bbe76aa8eaeecb3a
Matteo Nunziati <eng.matteo.nunziati@gmail.com>
2021-11-14 13:40:10 +0100

    app: remove search logic from client search serv.

--- app/src/services/RoleSearchService.ts
+++ app/src/services/RoleSearchService.ts
@@ -2,8 +2,6 @@ import { RoleCollection } from "../collections/RoleCollection";
 import type { Role } from "../models/role"
 
 export class RoleSearchService {
-   private filteredRoles: Array<Role> = [];
-
    private rolesC: RoleCollection
 
    constructor() {
@@ -11,49 +9,16 @@ export class RoleSearchService {
    }
 
    async handleSearch(searchString: string): Promise<Array<Role>> {
-      // stub for server side seach - does nothing for now and passes to client side search
+      // stub for server side search - does nothing for now and passes to client side search
       let roles = await this.rolesC.search(searchString)
 
-      // init stats
-      roles.forEach(role => role.resetMatches())
-
-      // nothing to do here, just return the whole roles set
-      if (searchString === "") {
-         //this.filteredRoles = [...this.rolesC.roles]
-         this.filteredRoles = [...roles]
-         return this.filteredRoles
-      }
-
-      // get serch terms from search bar, each separated by space
-      let searchTerms = searchString.split(" ")
-      // sort search so that, if not all the criteria are matched we can order 
-      // alphabetically by first maching term later
-      searchTerms.sort() 
-      
-      // scan roles for each search term, this manipulates the roles values
-      searchTerms.forEach(term => this.searchSingleTerm(roles, term))
-
-      // extract only those roles whose match the search terms
-      this.filteredRoles = roles.filter( role => {
-         // compute percentage of permissions matching against the search term
-         try {
-            if (role.includedPermissions.length > 0)
-               role.perc_match += role.matches / role.includedPermissions.length
-         
-        } catch {
-           console.log(role)
-        }
-
-         return (role.perc_match > 0) ? role : null
-      })
-
-      if (this.filteredRoles.length === 0) {
+      if (roles.length === 0) {
          alert("No result found!")
          return []
       }
 
       // order by matching
-      this.filteredRoles.sort( (first: Role, second: Role) => {
+      roles.sort( (first: Role, second: Role) => {
          // sort by number of matches
          let criterium = second.matchedBy.length - first.matchedBy.length 
          if (criterium != 0)
@@ -76,27 +41,6 @@ export class RoleSearchService {
          return criterium
       })
 
-      return this.filteredRoles
-   }
-
-   private searchSingleTerm(roles: Array<Role>, searchTerm: string) {
-      let term = new RegExp(searchTerm)
-      
-      roles.forEach(role => {
-         //console.log(role)
-         if (role.includedPermissions === undefined)
-            return
-
-         // let see if we match this search term
-         let matchingPerms = role.includedPermissions.filter(perm => {
-            return term.test(perm) ? true : false
-         })
-
-         //add the number of matches for this search term
-         if (matchingPerms.length > 0) {
-            role.matches += matchingPerms.length
-            role.matchedBy.push(searchTerm)
-         }
-      })
+      return roles
    }
 }
