d59776f38de992533e57a79043af3a7a08889c7a
Matteo Nunziati <eng.matteo.nunziati@gmail.com>
2021-11-21 18:17:03 +0100

    srv: made searchservice tests atomic

--- server/services/searchService_test.go
+++ server/services/searchService_test.go
@@ -6,7 +6,7 @@ import (
 	"testing"
 )
 
-func TestSearchRole(t *testing.T) {
+func TestSearchRoleDefaultValues(t *testing.T) {
 	// default values
 	var testSearchString string
 	var testDB *data.DB
@@ -15,25 +15,37 @@ func TestSearchRole(t *testing.T) {
 	if roles != nil || err == nil {
 		t.Fatalf("nil DB pointer doesn't return error")
 	}
+}
 
+func TestSearchRoleStringInited(t *testing.T) {
 	// string custom but db default
-	testSearchString = "compute"
-	roles, err = SearchRole(testSearchString, testDB)
+	var testSearchString = "compute"
+	var testDB *data.DB
+
+	roles, err := SearchRole(testSearchString, testDB)
 	if roles != nil || err == nil {
 		t.Fatalf("nil DB pointer doesn't return error")
 	}
+}
 
+func TestSearchRoleDBInited(t *testing.T) {
 	// string default, DB inited
-	var testSearchString2 string
-	testDB, _ = data.NewDB()
-	roles, err = SearchRole(testSearchString2, testDB)
+	var testSearchString string
+	testDB, _ := data.NewDB()
+
+	roles, err := SearchRole(testSearchString, testDB)
 	if len(roles) != len(testDB.Roles) || err != nil {
 		t.Fatalf("empty string should return no error and the entire DB")
 	}
+}
 
+func TestSearchRoleAllInited(t *testing.T) {
 	//both properly inited - here we have to mock the repo...
 	//TODO: this is a wrong test: depending on the string result could be empty
-	roles, err = SearchRole(testSearchString, testDB)
+	var testSearchString = "compute"
+	testDB, _ := data.NewDB()
+
+	roles, err := SearchRole(testSearchString, testDB)
 	if len(roles) <= 0 || len(roles) >= len(testDB.Roles) || err != nil {
 		t.Fatalf("inited string should return no error and a fraction of the DB")
 	}
