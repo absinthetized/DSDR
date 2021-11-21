172786c60dca3b12d97066770284dc6b99f5a026
Matteo Nunziati <eng.matteo.nunziati@gmail.com>
2021-11-21 18:37:05 +0100

    srv: refactored DB to improve coverage

--- server/data/database_test.go
+++ server/data/database_test.go
@@ -5,8 +5,22 @@ import (
 )
 
 func TestDBParser(t *testing.T) {
-	roles, err := db_parser()
+	roles, err := db_parser(".")
 	if len(roles) == 0 || err != nil {
 		t.Fatalf("DB should load roles and return no error")
+	}
+}
+
+func TestDBParserFailOnFolderAbsPath(t *testing.T) {
+	roles, err := db_parser("@pippo//\\")
+	if len(roles) != 0 || err == nil {
+		t.Fatalf("DB should fail with error on wrong path input")
+	}
+}
+
+func TestDBParserFailOnRadDir(t *testing.T) {
+	roles, err := db_parser("/")
+	if len(roles) != 0 || err == nil {
+		t.Fatalf("DB should fail with error on unauthorized or non readable folder")
 	}
 }
