9649eb59483abd05f59120e289049a4dfb0475d1
matteo nunziati <eng.matteo.nunziati@gmail.com>
2021-12-12 11:01:37 +0100

    app: celaned up Role pojo as search is server side

--- app/src/models/role.ts
+++ app/src/models/role.ts
@@ -3,22 +3,6 @@ a class to statically store properties of a gcp role
 mainly to be used by a RoleCollection
 */
 export class Role {
-
-        // example:
-        // "description": "Ability to view or act on access approval requests and view configuration",
-        // "includedPermissions": [
-        //   "accessapproval.requests.approve",
-        //   "accessapproval.requests.dismiss",
-        //   "accessapproval.requests.get",
-        //   "accessapproval.requests.list",
-        //   "accessapproval.settings.get",
-        //   "resourcemanager.projects.get",
-        //   "resourcemanager.projects.list"
-        // ],
-        // "name": "roles/accessapproval.approver",
-        // "stage": "BETA",
-        // "title": "Access Approval Approver"
-
     // these come from backend
     public readonly description: string;
     public readonly name: string;
@@ -26,8 +10,6 @@ export class Role {
     public readonly title: string;
     public readonly includedPermissions: Array<string>;
     public readonly id: number;
-
-    // these are computed
     public matches: number
     public matchedBy: string[]
     public perc_match: number
@@ -35,16 +17,5 @@ export class Role {
     constructor(item: any) {
         // this comes from server side
         Object.assign(this, item)
-
-        //client side compiled
-        this.matches = 0
-        this.matchedBy = []
-        this.perc_match = 0
-    }
-
-    resetMatches() {
-        this.matchedBy = []
-        this.matches = 0
-        this.perc_match = 0
-    }
+   }
 }
