3850e7b30e5e886ae3b60d47157adb8d854295ea
matteo nunziati <eng.matteo.nunziati@gmail.com>
2021-12-29 14:16:00 +0100

    app: added testing library svelte deps and config

--- app/jest.config.js
+++ app/jest.config.js
@@ -2,9 +2,14 @@
 module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
+  // for coverage enforcing
   coverageThreshold: {
     global: {
       lines: 75
     }
+  },
+  // for svelte component test via testing library
+  transform: {
+    "^.+\\.svelte$": "svelte-jester"
   }
 };
