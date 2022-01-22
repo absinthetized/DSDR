19ef47134c28a399fcc1808a225c4977f680b69c
matteo nunziati <eng.matteo.nunziati@gmail.com>
2022-01-22 18:25:18 +0100

    app: rewritten the svelte App.svelte to make use if the router

--- app/src/App.svelte
+++ app/src/App.svelte
@@ -1,61 +1,20 @@
-<script lang="ts">
-   import RoleComponent from "./components/RoleComponent.svelte";
-   import { RoleSearchService } from "./services/RoleSearchService";
-   import RoleSearchBar from "./components/RoleSearchBarComponent.svelte";
-   import FilterComponent from "./components/FilterComponent.svelte";
-   import { Navbar, NavbarBrand } from "sveltestrap";  
-   import { FilterService } from "./services/FilterService";
-   import { RoleCollection } from "./collections/RoleCollection";
+<script lang='ts'>
+import { Navbar, NavbarBrand } from "sveltestrap"
+import Router from 'svelte-spa-router'
+import {wrap} from 'svelte-spa-router/wrap'
 
-   const roleC = new RoleCollection()
-   const roleSearchEngine = new RoleSearchService(roleC)
-   const filter = new FilterService()
-   let searchTerm = ""
-   let roles = []
+const routes = {
+    '/': wrap({ asyncComponent: () => import("pages/Home.svelte") }),
+    '/search': wrap({ asyncComponent: () => import("pages/MainPage.svelte") }),
 
-   async function handleSearch(event: CustomEvent) {
-      searchTerm = event.detail.searchFor;
-      roles = [] //forces reinit of array and fixes repaint of badges for GA/BETA/...
-      roles = await roleSearchEngine.handleSearch(searchTerm)
-      // console.log("found so many matches: " + roleSearchEngine.filteredRoles.length)
-   }
+    // Catch-all
+    '*': wrap({ asyncComponent: () => import("pages/NotFound.svelte") }),
+}
 </script>
 
 <div class="container-fluid gx-5">
    <Navbar color="light" light expand="md">
       <NavbarBrand href="/">Welcome to the DSDR!</NavbarBrand>
    </Navbar>
-
-   <RoleSearchBar on:searchMessage={handleSearch} />
-   <FilterComponent 
-      bind:doAlpha={filter.doAlpha}
-      bind:doBeta={filter.doBeta}
-      bind:doDeprec={filter.doDeprec}
-      bind:doMinPerc={filter.doMinPerc}
-   ></FilterComponent>
-   <br />
-
-   {#each roles as role}
-      {#if (
-            (role.stage === 'ALPHA' && filter.doAlpha) || 
-            (role.stage === 'BETA' && filter.doBeta) ||
-            (role.stage === 'DEPRECATED' && filter.doDeprec) ||
-            (role.stage === 'GA')
-           ) && (role.perc_match*100) >= filter.doMinPerc
-      }
-      <RoleComponent
-         name={role.name}
-         description={role.description}
-         title={role.title}
-         stage={role.stage}
-         includedPermissions={role.includedPermissions}
-         searchedBy={role.matchedBy}
-         matches = {role.matches}
-         id = {role.id}
-         perc_matches ={(role.perc_match * 100).toFixed(2)}
-      />
-      {/if}
-   {:else}
-      <h2 style="text-align: center;">Perform you search!</h2>
-   {/each}
+   <Router {routes}/>
 </div>
