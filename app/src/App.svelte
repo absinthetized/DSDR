9ee0ba9af3ad367ee91e2f3417d035b5c8c677a3
matteo nunziati <eng.matteo.nunziati@gmail.com>
2022-01-22 18:29:21 +0100

    app: added main manu entries to navbar

--- app/src/App.svelte
+++ app/src/App.svelte
@@ -1,5 +1,5 @@
 <script lang='ts'>
-import { Navbar, NavbarBrand } from "sveltestrap"
+import { Navbar, NavbarBrand, Nav, NavLink } from "sveltestrap"
 import Router from 'svelte-spa-router'
 import {wrap} from 'svelte-spa-router/wrap'
 
@@ -15,6 +15,12 @@ const routes = {
 <div class="container-fluid gx-5">
    <Navbar color="light" light expand="md">
       <NavbarBrand href="/">Welcome to the DSDR!</NavbarBrand>
+      <Nav>
+         <NavLink href="#/">Home</NavLink>
+         <NavLink href="#/search">Search</NavLink>
+         <NavLink disabled href="#/reindex">Reindex DB</NavLink>
+         <NavLink disabled href="#/logout">Logout</NavLink>
+       </Nav>
    </Navbar>
    <Router {routes}/>
 </div>
