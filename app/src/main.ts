1777331f4a914694022fdee10a1e78523a22d8bb
matteo nunziati <eng.matteo.nunziati@gmail.com>
2022-02-06 17:26:28 +0100

    app: made auth state event setup  user singleton

--- app/src/main.ts
+++ app/src/main.ts
@@ -14,24 +14,17 @@ const fapp = initializeApp(firebaseConfig);
 // Install sign-in/sign-out event listener
 
 import { getAuth, onAuthStateChanged } from "firebase/auth";
-import type { User } from "firebase/auth";
-
-let currentUser: User
+import currentUser from "./models/user"
 
 const auth = getAuth();
-onAuthStateChanged(auth, (user) => {
-  if (user) {
-    // User is signed in, see docs for a list of available properties
-    // https://firebase.google.com/docs/reference/js/firebase.User
-    const uid = user.uid;
-    // ...
-
-    currentUser = user
-    console.log('current user is: ' + currentUser.email)
-  } else {
-    // User is signed out
-    // ...
+onAuthStateChanged(auth, (userInfo) => {
+  if (userInfo) {
+    currentUser.info = userInfo
+    console.log('current user is: ' + currentUser.info.email)
+  
+} else {
     console.log('no user currently logged in')
+	currentUser.info = null
   }
 });
 
