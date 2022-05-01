866dd27949879f49c5c0f4ff8170767836a38c8c
matteo nunziati <eng.matteo.nunziati@gmail.com>
2022-05-01 14:19:50 +0200

    srv: redesigned data models hierarchy

--- server/models/role.go
+++ server/models/role.go
@@ -1,13 +1,37 @@
 package models
 
+import "strings"
+
+// basic type to be derived/extended
+type abstractIAMRole struct {
+	Description string `json:"description"`
+	Name        string `json:"name"`
+	Stage       string `json:"stage"`
+	Title       string `json:"title"`
+	Id          int    `json:"id"`
+}
+
+// BqIAMRole maps the info of GCP IAM roles stored in big query
+type BqIAMRole struct {
+	abstractIAMRole
+
+	Included_permissions string // bq doesn't store arrays in a field
+}
+
 // BasicIAMRole models the json info of GCP IAM roles
+// it is expected to be a method in place able to map the BqIAMRole Included_permissions string
+// of comma separeted roles into an array of strings as the one in BasicIAMRole
 type BasicIAMRole struct {
-	Description         string   `json:"description"`
-	Name                string   `json:"name"`
-	Stage               string   `json:"stage"`
-	Title               string   `json:"title"`
+	abstractIAMRole
+
 	IncludedPermissions []string `json:"includedPermissions"`
-	Id                  int      `json:"id"`
+}
+
+func NewIAMfromBq(bq BqIAMRole) *BasicIAMRole {
+	role := new(BasicIAMRole)
+	role.abstractIAMRole = bq.abstractIAMRole
+	role.IncludedPermissions = append(role.IncludedPermissions, strings.Split(bq.Included_permissions, ",")...)
+	return role
 }
 
 // Role contains the IAM info of a Role plus some stats
