a3f0ddbc1ddc664a1bfd7d102dd308631551217f
matteo nunziati <eng.matteo.nunziati@gmail.com>
2022-05-29 11:13:51 +0200

    srv: intermediate  column filtering stuff

--- server/data/bqDatamapper.go
+++ server/data/bqDatamapper.go
@@ -1,33 +1,38 @@
 package data
 
 import (
+	"dsdr/models"
 	"log"
 	"reflect"
+	"strconv"
 	"strings"
 )
 
 type BqDatamapper[T any] struct {
-	table   string
-	db      *BqDB
-	cFilter string // cFilter is the column filter, basically (for now) the list of columns in a sql table
+	table      string
+	db         *BqDB
+	fieldNames []string
+	cFilter    string // cFilter is the column filter, basically (for now) the list of columns in a sql table
 }
+
+type BqQuery string
 
 func NewDataMapper[T any](db *BqDB, tablename string) *BqDatamapper[T] {
 	dm := new(BqDatamapper[T])
 	dm.db = db
 	dm.table = tablename
 
-	fieldNames := reflector(*new(T))
+	dm.fieldNames = reflector(*new(T))
 	for i := 0; i < len(fieldNames); i++ {
-		fieldNames[i] = strings.ToLower(fieldNames[i])
+		dm.fieldNames[i] = strings.ToLower(dm.fieldNames[i])
 	}
 
-	n := len(fieldNames) - 1
+	n := len(dm.fieldNames) - 1
 	for i := 0; i < n; i++ {
-		dm.cFilter = dm.cFilter + fieldNames[i] + ","
+		dm.cFilter = dm.cFilter + dm.fieldNames[i] + ","
 	}
 
-	dm.cFilter = dm.cFilter + fieldNames[n] // no trailing comma
+	dm.cFilter = dm.cFilter + dm.fieldNames[n] // no trailing comma
 
 	return dm
 }
@@ -42,6 +47,33 @@ func (dm *BqDatamapper[T]) Run(query BqQuery) ([]T, error) {
 	return Query[T](dm.db, query)
 }
 
+func (bq BqQuery) Where(filter models.BqIAMRole) BqQuery {
+	filtersMap := bq.reflectFilter(filter)
+	log.Println(filtersMap)
+
+	return bq
+}
+
+func (bq *BqQuery) reflectFilter(filter models.BqIAMRole) map[string]string {
+	var filterFields = make(map[string]string)
+
+	t := reflect.TypeOf(filter)
+	v := reflect.ValueOf(filter).FieldByName() //<-- ciò è figo!!!
+	for i := 0; i < v.NumField(); i++ {
+		if t.Field(i).Type.Kind() == reflect.Int {
+			filterFields[t.Field(i).Name] = strconv.Itoa(v.Field(i).Interface().(int))
+
+		} else if t.Field(i).Type.Kind() == reflect.String {
+			filterFields[t.Field(i).Name] = v.Field(i).Interface().(string)
+
+		} else {
+			log.Println("filtering for type", t.Field(i).Type.Kind(), "still missing sorry!")
+		}
+	}
+
+	return filterFields
+}
+
 func reflector(strct interface{}) []string {
 	var items []interface{}
 	var fieldNames []string
@@ -50,7 +82,7 @@ func reflector(strct interface{}) []string {
 	j := 0
 	for j < len(items) {
 		t := reflect.TypeOf(items[j])
-		log.Println("(A) reflecting", t.Name(), "of type", t.Kind()) //, "with # fields", t.NumField())
+		log.Println("(A) reflecting", t.Name(), "of type", t.Kind())
 
 		for i := 0; i < t.NumField(); i++ {
 			if t.Field(i).Type.Kind() == reflect.Struct {
